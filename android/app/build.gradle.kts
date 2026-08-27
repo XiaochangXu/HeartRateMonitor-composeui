6f202bce37cc46c7cedb0ff970dac6d3ed88473d
XiaochangXu <3693667273@qq.com>
2026-08-28 04:05:41 +0800

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.github.heartratemonitor_compose"
         minSdk = 24
         targetSdk = 37
-        versionCode = 51
-        versionName = "4.8.9"
+        versionCode = 52
+        versionName = "4.9.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
